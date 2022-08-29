e92fa920733fcc54ccd4c75d0dfedb2b2bb55e5c
jhonatan izidoro <jhonatanizidoro@gmail.com>
2022-08-29 11:20:06 -0300

    gradle updates

--- src/main/generated/com/comrades/app/application/mappers/MovieMapperImpl.java
+++ src/main/generated/com/comrades/app/application/mappers/MovieMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-25T00:25:10-0300",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16.0.2 (Amazon.com Inc.)"
+    date = "2022-08-29T11:02:55-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 @Component
 public class MovieMapperImpl implements MovieMapper {
